2f758cb16274475dc9fea426a4bc4e6872cad002
Ernie Hsiung <ehsiung@codeforamerica.org>
2015-04-02 10:17:51 -0700

    changing geocode API to https to fixed blocked request

--- src/geocode.js
+++ src/geocode.js
@@ -1,7 +1,7 @@
 'use strict'
 /* global $ */
 
-var GOOGLE_MAPS_URL = 'http://maps.googleapis.com/maps/api/geocode/json'
+var GOOGLE_MAPS_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
 
 // Google Maps Geocoding API documentation
 // https://developers.google.com/maps/documentation/geocoding/
